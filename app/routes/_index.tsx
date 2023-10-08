06c43484408477153b37d978031b2995c5954be4
Tomás García Gobet <t@gobet.ar>
2023-10-08 20:18:13 +0200

    sets up table component

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,58 @@
-import type { MetaFunction } from "@remix-run/node";
+import { json, type MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { prisma } from "~/db.server";
 
 export const meta: MetaFunction = () => [{ title: "Movies" }];
 
+export async function loader() {
+  const movies = await prisma.movie.findMany({ take: 10 });
+
+  return json(movies);
+}
+
 export default function Index() {
+  const movies = useLoaderData<typeof loader>();
   return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-      <span>Hello</span>
+    <main className="antialiased text-slate-500 dark:text-slate-400 bg-white dark:bg-slate-900 p-4 h-full">
+      <div className="not-prose relative bg-slate-50 rounded-xl overflow-hidden dark:bg-slate-800/25">
+        <div className="shadow-sm overflow-hidden my-8">
+          <table className="border-collapse table-auto w-full text-sm">
+            <thead>
+              <td className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                Title
+              </td>
+              <td className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                Year
+              </td>
+              <td className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                Genre
+              </td>
+              <td className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                Extract
+              </td>
+            </thead>
+
+            <tbody className="bg-white dark:bg-slate-800">
+              {movies.map((movie) => (
+                <tr key={movie.id}>
+                  <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                    {movie.title}
+                  </td>
+                  <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                    {movie.year}
+                  </td>
+                  <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                    {movie.genre}
+                  </td>
+                  <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                    {movie.extract}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </main>
   );
 }
